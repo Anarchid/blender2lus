9d144eb44871a616493d5490b1bcd38d2699f80c
Анархид <anarchid@mail.ru>
2019-09-19 16:19:02 +0300

    fix picasso being picasso again

--- io_anim_lus/__init__.py
+++ io_anim_lus/__init__.py
@@ -18,7 +18,7 @@
 
 bl_info = {
     "name": "Export: SpringRTS Lua Unit Script (LUS)",
-    "author": "An orchid",
+    "author": "Anarchid",
 	"version" : (0,1,2),
     "blender": (2, 80,0),
     "location": "File > Import-Export",
